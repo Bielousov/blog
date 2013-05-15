d380f56eb89b342db2b5348c1422dace5544b663
Anton Bielousov <anton@mobify.com>
2013-05-15 11:53:28 -0700

    Show full page width Google Ad
    Updated Google ad banner size to 970x90

--- ad_google.php
+++ ad_google.php
@@ -1,11 +1,11 @@
 <br/>
 <script type="text/javascript"><!--
 	google_ad_client = "ca-pub-8242423139183769";
-	/* Непутевая Канада */
-	google_ad_slot = "0735526271";
-	google_ad_width = 728;
+	/* Blog Wide */
+	google_ad_slot = "3095882392";
+	google_ad_width = 970;
 	google_ad_height = 90;
-	//-->
+//-->
 </script>
 <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 <br/>
