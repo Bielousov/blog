373dabd8fb83cb6b25515ea9e7eddc9e404c5b2b
Anton Bielousov <anton@mobify.com>
2013-08-13 11:59:41 -0700

    2.1.12 release

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<link rel="author" href="https://plus.google.com/104977961152292400509?rel=author" title="Anton Bielousov"/>
 	<?php
 	    $custom_fields = get_post_custom();
-	    wp_enqueue_style('ui', get_template_directory_uri() . '/css/style.css', false, '2.1.10', 'all');
+	    wp_enqueue_style('ui', get_template_directory_uri() . '/css/style.css', false, '2.1.12', 'all');
 	    wp_enqueue_script('ui', WP_CONTENT_URL . '/themes/myblog/scripts/ui.js',  array('jquery'));
 
 		/* We add some JavaScript to pages with the comment form
