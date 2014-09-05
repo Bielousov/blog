537572c6fd48fa6310eae40e5c3ec3d223373e35
Anton Bielousov <anton@mobify.com>
2014-09-05 12:28:20 -0700

    Fixed comments count for wordpress 4.0

--- _entry-utility.php
+++ _entry-utility.php
@@ -15,19 +15,25 @@ $custom_fields = get_post_custom();
 		<?php
 			// Comments counter
 			if(comments_open()) {
+				$commentsCount = 0;
+				$commentsClassName = 'x-entry-utility__comments-link';
+
 				if(!is_single() && get_post_format() != 'link' ) {
-					// Display comments counter
-					printf( __( '<a href="%1$s" class="%2$s">%3$s</a>'), get_permalink().'#comments', 'x-entry-utility__comments-link', get_comments_number('+', '1', '%'));
+					$commentsCount = get_comments_number();
 				} elseif(get_post_format() == 'link' && isset($custom_fields['target-post'][0])) {
 					// If linked post, get comments counter of targeted post
-					printf( __( '<a href="%1$s" class="%2$s">%3$s</a>'), get_permalink().'#comments', 'x-entry-utility__comments-link x--target-post', get_comments_number(intval($custom_fields['target-post'][0]), '+', '1', '%'));
+					$commentsCount = get_comments_number(intval($custom_fields['target-post'][0]));
+					$commentsClassName .= ' x--target-post';
 				}
+
+				// Display comments counter
+				printf( __( '<a href="%1$s" class="%2$s">%3$s</a>'), get_permalink().'#comments', $commentsClassName, $commentsCount);
 			}
 
 			// Post edit link
 			edit_post_link( __( 'Редактировать', 'myblog' ), '<span class="x-entry-utility__edit-link">', '</span>' );
 
-		
+
 			// Read More Link
 			if(!is_single()) {
 				printf( __( '<a href="%1$s" class="%2$s">%3$s</a>'), get_permalink(), 'x-entry-utility__more-link', __( myblog_more_text(), 'myblog' ));
