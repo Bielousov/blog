8c6d2291b17359db3f656df838b0f1c7017cb008
Anton Bielousov <anton@mobify.com>
2014-12-05 10:39:56 -0800

    Adde nofollow to sponsored category link

--- _entry-meta.php
+++ _entry-meta.php
@@ -8,6 +8,16 @@
 ?>
 
 <div class="x-entry-meta">
-	<?php myblog_posted_on(); ?>
-	<?php printf( __( '<span class="%1$s">/ %2$s', 'myblog' ), 'x-entry-meta__category', get_the_category_list( ', ' ) ); ?>
+	<?php
+        myblog_posted_on();
+
+        $categoryLinks = get_the_category_list( ', ' );
+
+        // Advirtiser request: add rel="nofollow" to sponsored post category link
+        if (in_category( 'sponsored' )) {
+            $categoryLinks = str_replace('rel="category', 'rel="nofollow category', $categoryLinks);
+        }
+
+        printf( __( '<span class="%1$s">/ %2$s', 'myblog' ), 'x-entry-meta__category', $categoryLinks );
+    ?>
 </div>
