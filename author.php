2f98600b901f0eac3f0b4508619e219d0c74cf51
Anton Bielousov <anton@mobify.com>
2015-03-03 10:48:13 -0800

    Fixed new header/footer on all pages

--- author.php
+++ author.php
@@ -3,12 +3,21 @@
  * The template for displaying Author Archive pages.
  *
  * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+ * @subpackage MyBlog
+ * @since 3.0
  */
 
-get_header(); ?>
+    get_template_part( '_head' );
+?>
 
+<body <?php body_class(); ?>>
+
+<?php
+    get_header();
+?>
+
+<div id="wrapper" class="hfeed">
+    <div id="main" class="content-wrapper">
 		<div id="container">
 			<div id="content" role="main">
 
@@ -26,35 +35,44 @@ get_header(); ?>
 
 				<h1 class="author page-title rublog-title--category"><?php printf( __( 'Author Archives: %s', 'myblog' ), "<span class='vcard'><a class='url fn n' href='" . get_author_posts_url( get_the_author_meta( 'ID' ) ) . "' title='" . esc_attr( get_the_author() ) . "' rel='me'>" . get_the_author() . "</a></span>" ); ?></h1>
 
-<?php
-// If a user has filled out their description, show a bio on their entries.
-if ( get_the_author_meta( 'description' ) ) : ?>
-					<div id="entry-author-info">
-						<div id="author-avatar">
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'myblog_author_bio_avatar_size', 60 ) ); ?>
-						</div><!-- #author-avatar -->
-						<div id="author-description">
-							<h2><?php printf( __( 'About %s', 'myblog' ), get_the_author() ); ?></h2>
-							<?php the_author_meta( 'description' ); ?>
-						</div><!-- #author-description	-->
-					</div><!-- #entry-author-info -->
-<?php endif; ?>
+				<?php
+				// If a user has filled out their description, show a bio on their entries.
+					if ( get_the_author_meta( 'description' ) ) : ?>
+						<div id="entry-author-info">
+							<div id="author-avatar">
+								<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'myblog_author_bio_avatar_size', 60 ) ); ?>
+							</div><!-- #author-avatar -->
+							<div id="author-description">
+								<h2><?php printf( __( 'About %s', 'myblog' ), get_the_author() ); ?></h2>
+								<?php the_author_meta( 'description' ); ?>
+							</div><!-- #author-description	-->
+						</div><!-- #entry-author-info -->
+				<?php endif; ?>
 
-<?php
-	/* Since we called the_post() above, we need to
-	 * rewind the loop back to the beginning that way
-	 * we can run the loop properly, in full.
-	 */
-	rewind_posts();
+				<?php
+					/* Since we called the_post() above, we need to
+					 * rewind the loop back to the beginning that way
+					 * we can run the loop properly, in full.
+					 */
+					rewind_posts();
 
-	/* Run the loop for the author archive page to output the authors posts
-	 * If you want to overload this in a child theme then include a file
-	 * called loop-author.php and that will be used instead.
-	 */
-	 get_template_part( 'loop', 'author' );
-?>
+					/* Run the loop for the author archive page to output the authors posts
+					 * If you want to overload this in a child theme then include a file
+					 * called loop-author.php and that will be used instead.
+					 */
+					 get_template_part( 'loop', 'author' );
+				?>
 			</div><!-- #content -->
 		</div><!-- #container -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+		<?php get_sidebar(); ?>
+
+    </div><!-- #main -->
+
+    <?php get_footer(); ?>
+
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+
+</body></html>
