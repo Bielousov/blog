091e775400a4bf66c2df33ff713069e7465f927f
Anton Bielousov <anton@mobify.com>
2015-09-01 11:01:03 -0700

    Removed translation header

--- header.php
+++ header.php
@@ -2,30 +2,11 @@
 	<div class="header-wrapper">
 		<hgroup class="content-wrapper notranslate">
 			<div class="rublog-title--blog">
-				<?php  if(is_home() || is_front_page()) { ?>
-					<h1 data-lang="ru">
-						<ins></ins>
-						<big>Непутевая Канада</big>
-						<small>Блог <a href="/author" rel="author">Антона Белоусова</a></small>
-					</h1>
-					<h1 data-lang="en" class="rublog-title--blog">
-						<ins></ins>
-						<big>Shiftless Canada</big>
-						<small><a href="/" rel="home">www.rublog.ca</a></small>
-					</h1>
-				<?php } else { ?>
-					<div  data-lang="ru">
-						<ins></ins>
-						<big><a href="/" rel="home">Непутевая Канада</a></big>
-						<small>Блог <a href="/author" rel="author">Антона Белоусова</a></small>
-					</div>
-					<div  data-lang="en" class="rublog-title--blog">
-						<ins></ins>
-						<big><a href="/" rel="home">Oooh, Canada</a></big>
-						<small><a href="/" rel="home">www.rublog.ca</a></small>
-					</div>
-				<?php } ?>
-
+				<h1>
+					<ins></ins>
+					<big><a href="/" rel="home">Непутевая Канада</a></big>
+					<small>Блог <a href="/author" rel="author">Антона Белоусова</a></small>
+				</h1>
 				<a class="rss-feed" href="http://feeds.feedburner.com/Bielousov" target="_blank" rel="nofollow" onclick="return trackOutboundLink(this, 'RSS', 'Click RSS Link', false, false);" rel="nofollow" title="Подпишитесь на RSS-рассылку и не пропускайте ни одного поста."></a>
 			</div>
 
