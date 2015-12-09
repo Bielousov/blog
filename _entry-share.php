1c71e9790c361abbfc2eeaf884dedf6d8f753dc1
Anton Bielousov <anton@mobify.com>
2015-12-09 10:39:09 -0800

    Added Subscription link to share block

--- _entry-share.php
+++ _entry-share.php
@@ -7,4 +7,5 @@
 <div class="x-share">
     <h6 class="x-share__title">Лучший способ сказать «спасибо»:</h6>
 	<?php echo do_shortcode('[shareaholic app="share_buttons" id="17129403"]'); ?>
+    <p><a href="http://eepurl.com/bEIrRb" class="x-share__link" target="_blank" rel="nofollow">Подпишитесь на рассылку новых постов по почте</a></p>
 </div>
