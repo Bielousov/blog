e09ef2cbb089b443ac4df79da38020e710d05bcd
Anton Bielousov <anton@mobify.com>
2016-02-03 10:49:05 -0800

    Fixed google ads background colour

--- ad_google.php
+++ ad_google.php
@@ -1,7 +1,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Blog All Posts (responsive) -->
 <ins class="adsbygoogle"
-     style="display:block"
+     style="display:block; background:white;"
      data-ad-client="ca-pub-8242423139183769"
      data-ad-slot="5977600798"
      data-ad-format="auto"></ins>
