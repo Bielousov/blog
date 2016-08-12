33fa342af9fb7e2d57250a0384fca1c7500a736d
Anton Bielousov <anton@mobify.com>
2016-08-12 10:53:05 -0700

    Added required hentry meta data

--- functions.php
+++ functions.php
@@ -330,6 +330,18 @@ function ru_plural($num, $words, $print_num=false){
 	return ($print_num ? $num . '&nbsp;' : '') . $_w;
 }
 
+//add hatom data
+function add_suf_hatom_data($content) {
+    $t = get_the_modified_time('F jS, Y');
+    $author = get_the_author();
+    $title = get_the_title();
+	if (is_home() || is_singular() || is_archive() ) {
+        $content .= '<div class="hatom-extra" hidden><span class="entry-title">'.$title.'</span> was last modified: <span class="updated"> '.$t.'</span> by <span class="author vcard"><span class="fn">Антон Белоусов</span></span></div>';
+    }
+    return $content;
+}
+add_filter('the_content', 'add_suf_hatom_data');
+
 // Use Twitter photo card type with Yoast SEO plugin
 add_filter( 'wpseo_twitter_card_type', 'change_card_type', 20 );
 function change_card_type(  ) {
