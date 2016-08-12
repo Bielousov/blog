02f85ffc65362b63c4d6eafa56ef3758cb2334af
Anton Bielousov <anton@mobify.com>
2016-08-12 10:52:32 -0700

    Updated Share CTA

--- _entry-share.php
+++ _entry-share.php
@@ -5,7 +5,7 @@
 ?>
 
 <div class="x-share">
-    <h6 class="x-share__title">Лучший способ сказать «спасибо»:</h6>
+    <h6 class="x-share__title">Лучший способ поддержать этот блог — поделиться ссылкой:</h6>
 	<?php echo do_shortcode('[shareaholic app="share_buttons" id="17129403"]'); ?>
     <p><a href="http://eepurl.com/bEIrRb" class="x-share__link" target="_blank" rel="nofollow">Подпишитесь на рассылку новых постов по почте</a></p>
 </div>
