0cf7c4cd57d7914222e5c0bbe4c776645baf0621
Anton Bielousov <anton@mobify.com>
2017-02-17 11:29:37 -0800

    Added comments, related posts, report error, tweat ideas and Google adds to default page template

--- loop-page.php
+++ loop-page.php
@@ -41,9 +41,48 @@
 			// Entry Meta
 			get_template_part( '_entry-utility');
 
+            // Ads
+            if(!isset($custom_fields['ad-free'])) {
+                get_template_part( 'ad_google' );
+            }
+
             // Entry Share
             get_template_part( '_entry-share');
 		?>
+
+        <?php
+            // Tweet ideas
+            if(isset($custom_fields['twitter_idea']) && !empty($custom_fields['twitter_idea'])) {
+                echo '<div class="rublog-widget--social-tweet"><h5 class="rublog-title--widget">Идеи для твитов:</h5><div class="rublog-widget__content"><ul>';
+                for($i=0; $i < count($custom_fields['twitter_idea']); $i++) {
+                    echo '<li><a class="twitter-share-button rublog-widget__cta" data-count="none" data-text="'.$custom_fields['twitter_idea'][$i].'" data-via="Bielousov" rel="nofollow"></a>'
+                            .'&laquo;'.$custom_fields['twitter_idea'][$i].'&raquo;'
+                            .'</li>';
+                }
+                echo '</ul></div></div>';
+                echo '<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
+            }
+        ?>
+
+        <?php
+            // Errors notification
+            if(strlen(strip_tags(strip_shortcodes(get_the_content()))) > 100) {
+                echo '<div id="typo-notification" class="sans-serif">'
+                    .'<h5 class="rublog-title--widget">Нашли ошибку?</h5>'.
+                        error_notification_action_text()
+                    .'</div>';
+            }
+        ?>
 	</article><!-- #post-## -->
+<!-- Wrap Container Flow -->
+        </div><!-- #content -->
+    </div><!-- #container -->
+</div><!-- #main -->
+    <div id="main-related" class="x-post-comments">
+        <div class="content">
+    <!-- Wrap Container Flow End -->
+
+        <?php comments_template( '', true ); ?>
+        <?php zemanta_related_posts()?>
 
 <?php endwhile; // end of the loop. ?>
